8f4726c000500536a04ff17cd9f25369be52f9b7
Jung Sung Hun <codingj87@zensystems.co.kr>
2020-07-30 15:25:47 +0900

    Add postgresql setting

--- myproject/config/settings.py
+++ myproject/config/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'NAME': 'aws',
+        'USER': 'codingj87',
+        'PASSWORD': 'zensystems!1'
     }
 }
 
